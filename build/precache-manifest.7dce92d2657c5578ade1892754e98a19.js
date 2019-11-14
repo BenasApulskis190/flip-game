7291fc9d01992e16d8b43d235bb21cf41c38a1fd
Benas Apulskis <benas@ito.lt>
2019-11-14 16:11:20 +0200

    Flip game build

---
+++ build/precache-manifest.7dce92d2657c5578ade1892754e98a19.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f6f4f636c55da5de2655",
+    "url": "/static/js/main.f6f4f636.chunk.js"
+  },
+  {
+    "revision": "186c5f0b49712e20072d",
+    "url": "/static/js/1.186c5f0b.chunk.js"
+  },
+  {
+    "revision": "f6f4f636c55da5de2655",
+    "url": "/static/css/main.168ebf88.chunk.css"
+  },
+  {
+    "revision": "43a62fadcae0d90fd2f5752a5c6f4f17",
+    "url": "/index.html"
+  }
+];
